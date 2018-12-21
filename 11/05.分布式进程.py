462faaa981b4ae7e578b5a5dfb9ffd6a24f78835
yanbo <2313567416@qq.com>
2018-12-21 18:54:05 +0800

    save

---
+++ 11/05.分布式进程.py
@@ -0,0 +1,37 @@
+# 在Thread和Process中，应当优选Process，因为Process更稳定，
+# 而且，Process可以分布到多台机器上，而Thread最多只能分布到同一台机器的多个CPU上
+
+
+
+# Python的multiprocessing模块不但支持多进程，其中managers子模块还支持把多进程分布到多台机器上。
+# 一个服务进程可以作为调度者，将任务分布到其他多个进程中，依靠网络通信。由于managers模块封装很好，
+# 不必了解网络通信的细节，就可以很容易地编写分布式多进程程序。
+#
+
+# 举个例子：如果我们已经有一个通过Queue通信的多进程程序在同一台机器上运行，现在，由于处理任务的进程任务繁重，
+# 希望把发送任务的进程和处理任务的进程分布到两台机器上。怎么用分布式进程实现？
+#
+# 原有的Queue可以继续使用，但是，通过managers模块把Queue通过网络暴露出去，就可以让其他机器的进程访问Queue了。
+# 我们先看服务进程，服务进程负责启动Queue，把Queue注册到网络上，然后往Queue里面写入任务：
+
+
+import  random, time, queue
+
+from multiprocessing.managers import BaseManager
+
+#发送任务的队列
+task_queue = queue.Queue()
+
+#接受结果的队列
+result_queue = queue.Queue()
+
+#从BaseManager继承的QueueManager
+
+class QueueManager(BaseManager):
+    pass
+
+
+#把两个Queue都注册到网络上， callbale参数关联了queue对象
+# QueueManager.register()
+
+
